bb48783a21dce44bb244f0268ec6f04a02f7e598
abdullahi <abdullahiyare17@gmail.com>
2023-12-18 11:51:30 +0300

    removed authentication error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { AuthError } from 'next-auth';
 
 export async function authenticate(
   prevState: string | undefined,
